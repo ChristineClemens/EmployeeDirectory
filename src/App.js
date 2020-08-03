87f05dcfca08adbb97adc80626903d7329bb6735
Christine Clemens <christineclemens.tesol@gmail.com>
2020-08-03 12:32:41 -0400

    Initialized App.js file.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+// import TableFilter from './components/TableFilter';
+// import TableOrder from './components/TableOrder';
+// import TableList from './components/TableList';
+// import employees from './employees.json'
+
+const App = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default App;
